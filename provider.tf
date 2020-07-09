fa58dae0deb926eecaaa1b8461a2d9f03f948d83
sankansm <santosh_sankannavar@yahoo.in>
2020-07-09 15:12:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,8 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "sankanams-terra-my-bucket"
-    key            = "elb-asg/terraform.tfstate"
+    bucket         = "terraform-remote-sankansm"
+    key            = "state/terraform.tfstate"
     region         = "us-east-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "sankanams-terra-my-table"
